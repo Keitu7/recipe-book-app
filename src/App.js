8ef067cc495982ead635e546c618171ac99fbc1c
Keitu <125948549+Keitu7@users.noreply.github.com>
2024-04-22 16:37:35 +0200

    Changed food categories

--- src/App.js
+++ src/App.js
@@ -7,13 +7,15 @@ function App() {
   const [recipes, setRecipes] = useState ([
     { id: 1, name: 'Salmon pasta', dietary: 'Non-vegetarian', cookingTime: '25 mins', favorite: false}, 
     { id: 2, name: 'Ponzu tofu', dietary: 'Vegetarian', cookingTime: '20 mins', favorite: false},
-    { id: 3, name: 'Chicken stir-fry', dietary: 'Vegetarian', cookingTime: '30 mins', favorite: false},
+    { id: 3, name: 'Chicken stir-fry', dietary: 'Vegan', cookingTime: '30 mins', favorite: false},
     { id: 4, name: 'Lentil Conconut curry', dietary: 'Non-vegetarian', cookingTime: '25 mins', favorite: false},
-    { id: 5, name: 'Healthy Gnocchi', dietary: 'Vegetarian', cookingTime: '30 mins', favorite: false},
+    { id: 5, name: 'Healthy Gnocchi', dietary: 'Vegan', cookingTime: '30 mins', favorite: false},
     { id: 6, name: 'Prawn fried rice', dietary: 'Non-vegetarian', cookingTime: '30 mins', favorite: false},
     { id: 7, name: 'Fajita chicken rice', dietary: 'Non-vegetarian', cookingTime: '25 mins', favorite: false},
-    { id: 8, name: 'Linguine with Avocado', dietary: 'Vegetarian', cookingTime: '30 mins', favorite: false},
+    { id: 8, name: 'Linguine with Avocado', dietary: 'Ketogenic', cookingTime: '30 mins', favorite: false},
     { id: 9, name: 'Veggie yaki udon', dietary: 'Non-vegetarian', cookingTime: '20 mins', favorite: false},
+    { id: 10, name: 'Keto-Brownie', dietary: 'Ketogenic', cookingTime: '22 mins', favorite: false},
+    { id: 11, name: 'Creamy Keto Tuscan', dietary: 'Ketogenic', cookingTime: '20 mins', favorite: false},
     //the array list of recipes
   ]);
 
@@ -27,7 +29,7 @@ function App() {
 
   const toggleFavourites = () => {
     setShowFavourites(!showFavourites);
-    //because it will deal with filtering ans showing only the favourites list
+    //because it will deal with filtering and showing only the favourites list
   };
 
   const handleFavouriteToggle  = (id) => {
@@ -40,6 +42,7 @@ function App() {
     setRecipes(updatedRecipes);
   };
 
+  //a const variable because to filter only according to dietary request will change according to searched request//
   const filteredRecipes = recipes.filter(recipe => {
     if (showFavourites && !recipe.favourite) {
       return false;
@@ -54,7 +57,7 @@ function App() {
     <>
       <h1>Recipe Cook Book</h1>
       <div>
-        <input type="text" placeholder="Search recipes..." onChange={handleFilterChange}></input>
+        <input id="text" placeholder="Search recipes..." onChange={handleFilterChange}></input>
         <button onClick={toggleFavourites}>{showFavourites ? 'Show All' : 'Show Favourites'}</button>
       </div>
       <RecipeList recipes={filteredRecipes} onFavouriteToggle={handleFavouriteToggle} />
